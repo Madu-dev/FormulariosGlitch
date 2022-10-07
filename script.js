45c3201710ffce8b120bff756ec43458423eb8fb
Maria do Carmo <69973218+Madu-dev@users.noreply.github.com>
2022-10-06 22:47:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+function validar() {
+    var email = document.getElementById('email').value;
 
-console.log("Hello, world!");
+    var comp = /\S+@\S+\.\S/;
+
+    if (email == '') {
+        alert("Preencha o e-mail!");
+    } else {
+        switch (comp.test(email)) {
+            case true:
+                alert("Digite um email válido!");
+                break
+
+            case false:
+                alert("Digite um email válido!");
+                break
+
+        }
+    }
+
+
+}
+
+function validar() {
+    var email = document.getElementById('e-mail').value;
+
+    var comp = /\S+@\S+\.\S/;
+
+    var email = document.getElementById('email');
+
+    email.addEventListener('focus', () => {
+        email.style.borderColor = "palevioletred";
+    });
+    email.addEventListener('blur', () => {
+        email.style.borderColor = "#ccc";
+    });
+
+    var senha = document.getElementById('senha');
+
+    senha.addEventListener('focus', () => {
+        senha.style.borderColor = "palevioletred";
+    });
+    senha.addEventListener('blur', () => {
+        senha.style.borderColor = "#ccc";
+    });
+}
